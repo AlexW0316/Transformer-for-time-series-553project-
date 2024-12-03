33c8b6f050eb3d241d710e00535419f4a90bbf25
Yufan Zhang <yufanzhang@Yufans-MacBook-Air.local>
2024-12-02 22:45:34 -0500

    Fix data_provider error: alldataset can't be sliced

--- EECS 553/data_provider.py
+++ EECS 553/data_provider.py
@@ -53,16 +53,14 @@ for i in range(window_size, n_dates):
     cur_dataset = TimeSeriesDataset(window_X, window_y, start_date, n_stocks)
     all_dataset = all_dataset + cur_dataset if all_dataset is not None else cur_dataset
 
+
 n_total = len(all_dataset)
 train_size = int(0.7 * n_total)
 valid_size = int(0.2 * n_total)
-dataset_train = all_dataset[:train_size]
-dataset_valid = all_dataset[train_size:train_size + valid_size]
-dataset_test = all_dataset[train_size + valid_size:]
+test_size = n_total - train_size - valid_size
+dataloader_train, dataloader_valid, dataloader_test =\
+    torch.utils.data.random_split(all_dataset, [train_size, valid_size, test_size])
+# # Returns [batch_size, window_size, n_features] and [batch_size, horizon] from a random sector
 
 batch_size = 32
 
-# Returns [batch_size, window_size, n_features] and [batch_size, horizon] from a random sector
-dataloader_train = DataLoader(dataset_train, batch_size=batch_size, shuffle=True)
-dataloader_valid = DataLoader(dataset_valid, batch_size=batch_size, shuffle=True)
-dataloader_test = DataLoader(dataset_test, batch_size=batch_size, shuffle=True)
