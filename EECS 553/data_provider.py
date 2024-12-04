0e4555552a3a9ccc5f580281fbfa2362341b21d3
Yufan Zhang <yufanzhang@Yufans-MacBook-Air.local>
2024-12-04 17:21:31 -0500

    Fix data_provider

--- EECS 553/data_provider.py
+++ EECS 553/data_provider.py
@@ -2,6 +2,7 @@ from torch.utils.data import DataLoader, Dataset
 import numpy as np
 import pandas as pd
 import torch
+from torch.utils.data import ConcatDataset
 
 
 class TimeSeriesDataset(Dataset):
@@ -43,15 +44,15 @@ X = torch.tensor(
 y = torch.tensor(price_df.values)  # [n_dates, n_stocks]
 
 window_size = 96
-horizon = 1
 
-all_dataset = None
-for i in range(window_size, n_dates):
+all_dataset = []
+for i in range(window_size, n_dates - (window_size + 20)):
     window_X = X[i - window_size:i, :, :]  # [window_size, n_stocks, n_features (=2)]
-    window_y = y[i:i + horizon, :]  # [horizon, n_stocks]
+    window_y = y[i + 20: i + window_size + 20, :]  # [window_size, n_stocks]
     start_date = dates[i]
     cur_dataset = TimeSeriesDataset(window_X, window_y, start_date, n_stocks)
-    all_dataset = all_dataset + cur_dataset if all_dataset is not None else cur_dataset
+    all_dataset.append(cur_dataset)
+all_dataset = ConcatDataset(all_dataset)
 
 n_total = len(all_dataset)
 train_size = int(0.7 * n_total)
@@ -62,5 +63,5 @@ dataset_train, dataset_valid = \
 batch_size = 32
 dataloader_train, dataloader_valid = \
     DataLoader(dataset_train, batch_size=batch_size, shuffle=True), \
-    DataLoader(dataset_valid, batch_size=batch_size, shuffle=True), \
-# Returns [batch_size, window_size, n_features] and [batch_size, horizon] from a random sector
+        DataLoader(dataset_valid, batch_size=batch_size, shuffle=True), \
+    # Returns [batch_size, window_size, n_features] and [batch_size, horizon] from a random sector
