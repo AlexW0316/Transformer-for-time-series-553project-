9e49e033d6e1a5851d4d6a6f7c7d15415dffc975
Yufan Zhang <yufanzhang@Yufans-MacBook-Air.local>
2024-12-08 11:59:09 -0500

    Change folder name. Add apple stock.

--- Vanilla_transformer/data_provider.py
+++ Vanilla_transformer/data_provider.py
@@ -67,3 +67,45 @@ dataloader_train, dataloader_valid = \
     DataLoader(dataset_train, batch_size=batch_size, shuffle=True), \
         DataLoader(dataset_valid, batch_size=batch_size, shuffle=True), \
     # Returns [batch_size, window_size, n_features] and [batch_size, horizon] from a random sector
+
+
+
+# apple_stock
+
+
+price_df = pd.read_csv('apple_stock.csv', index_col=0)
+
+n_stocks = len(price_df.columns)
+dates = pd.to_datetime(price_df.index).strftime("%Y%m%d").astype(int)
+n_dates = len(dates)
+
+print()
+
+X = torch.tensor(price_df.values.reshape(-1, n_stocks, 1))  # [n_dates, n_stocks, n_features (=2)]
+
+y = torch.tensor(price_df.values)  # [n_dates, n_stocks]
+
+window_size = 96
+
+all_dataset = []
+for i in range(window_size, n_dates - (window_size + 20)):
+    window_X = X[i - window_size:i, :, :]  # [window_size, n_stocks, n_features (=2)]
+    window_y = y[i + 20: i + window_size + 20, :]  # [window_size, n_stocks]
+    start_date = dates[i]
+    cur_dataset = TimeSeriesDataset(window_X, window_y, start_date, n_stocks)
+    all_dataset.append(cur_dataset)
+all_dataset = ConcatDataset(all_dataset)
+
+
+n_total = len(all_dataset)
+print(n_total)
+train_size = int(0.7 * n_total)
+valid_size = n_total - train_size
+dataset_train, dataset_valid = \
+    torch.utils.data.random_split(all_dataset, [train_size, valid_size])
+
+batch_size = 32
+dataloader_train, dataloader_valid = \
+    DataLoader(dataset_train, batch_size=batch_size, shuffle=True), \
+        DataLoader(dataset_valid, batch_size=batch_size, shuffle=True), \
+    # Returns [batch_size, window_size, n_features] and [batch_size, horizon] from a random sector
